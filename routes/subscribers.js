8b1224cd38f0c237b2267371b0a76f89089c6ca7
ibraheemamer48 <ibraheemamer1998@hotmail.com>
2023-11-04 22:29:19 +0000

    first commmit

---
+++ routes/subscribers.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+// Getting all
+router.get('/', (req, res) => {
+    
+})
+// Getting one
+router.get('/:id', (req, res) => {
+
+})
+
+// Creating one
+router.post('', (req, res) => {
+    
+})
+
+// Updating One
+// Deleting one
+
+module.exports = router
