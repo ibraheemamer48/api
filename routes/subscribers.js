4eab516105089e57f068422b9c3482e1f0459d28
ibraheemamer48 <ibraheemamer1998@hotmail.com>
2023-11-11 19:24:25 +0000

    hello

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
+
+
 // Getting all
 router.get('/', (req, res) => {
-    
+  res.send('Hello World')  
 })
 // Getting one
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {  
 
 })
 
 // Creating one
-router.post('', (req, res) => {
+router.post('/', (req, res) => {
     
 })
 
 // Updating One
+
+router.patch('/', (req, res) => {
+
+})
+
 // Deleting one
+
+router.delete('/id:', (req, res) => {
+
+})
 
 module.exports = router
